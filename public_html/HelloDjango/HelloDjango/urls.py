5487785db45c55a2352df313b1ac33ba5c86c9f2
Laidboy <mr.laidboy@gmail.com>
2021-04-28 21:16:17 +0300

    db_first

--- public_html/HelloDjango/HelloDjango/urls.py
+++ public_html/HelloDjango/HelloDjango/urls.py
@@ -19,5 +19,6 @@ from firstapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('test/', views.index, name='home')
 ]
